9bb6ea741ff12bbf523ff6e4bf649c98033da270
Lyud Popov <lyud96@hotmail.co.uk>
2021-12-02 12:36:14 +0000

    Day 1.

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using System.Net.WebSockets;
+
+namespace AdventOfCode;
 
 public class Day_01 : BaseDay
 {
@@ -9,7 +11,60 @@ public class Day_01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        int increase = 0;
+        int? lastValue = null;
+
+        foreach (var strDepth in _input.Split(Environment.NewLine))
+        {
+            var depth = int.Parse(strDepth);
+
+            if (depth > lastValue)
+            {
+                increase++;
+            }
+
+            lastValue = depth;
+        }
+
+        return new ValueTask<string>(increase.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int increase = 0;
+
+        var inputs = _input.Split(Environment.NewLine);
+        var curPos = 1;
+        int? lastValue = null;
+
+        while (curPos < inputs.Length - 1)
+        {
+            var sums = new List<int>();
+
+            for (int numRuns = 0; numRuns < 2; numRuns++)
+            {
+                var curSum = 0;
+
+                for (var i = 0; i < 3 ; i++, curPos++)
+                {
+                    var depth = int.Parse(inputs[curPos - 1]);
+                    curSum += depth;
+                }
+
+                curPos -= 2;
+
+                if (curSum > lastValue)
+                {
+                    increase++;
+                }
+
+                lastValue = curSum;
+            }
+        }
+
+        return new ValueTask<string>(increase.ToString());
+    }
 }
