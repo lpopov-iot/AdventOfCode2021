bf0803d3f798f27e8f8a98947fac42a1de11579f
Lyud Popov <lyud96@hotmail.co.uk>
2021-12-02 15:25:43 +0000

    Day 2.

---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.WebSockets;
+
+namespace AdventOfCode;
+
+public class Day_02 : BaseDay
+{
+    private readonly string _input;
+
+    public Day_02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var depth = 0;
+        var horizontal = 0;
+
+        foreach (var line in _input.Split(Environment.NewLine))
+        {
+            var command = line.Split(" ");
+
+            var movement = command[0];
+            var magnitute = int.Parse(command[1]);
+
+            switch (movement)
+            {
+                case "forward":
+                    horizontal += magnitute;
+                    break;
+                case "down":
+                    depth += magnitute;
+                    break;
+                case "up":
+                    depth -= magnitute;
+                    break;
+            }
+        }
+
+        return new ValueTask<string>((depth * horizontal).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var aim = 0;
+        var depth = 0;
+        var horizontal = 0;
+
+        foreach (var line in _input.Split(Environment.NewLine))
+        {
+            var command = line.Split(" ");
+
+            var movement = command[0];
+            var magnitute = int.Parse(command[1]);
+
+            switch (movement)
+            {
+                case "forward":
+                    horizontal += magnitute;
+                    depth += (aim * magnitute);
+                    break;
+                case "down":
+                    aim += magnitute;
+                    break;
+                case "up":
+                    aim -= magnitute;
+                    break;
+            }
+        }
+
+        return new ValueTask<string>((depth * horizontal).ToString());
+    }
+}
